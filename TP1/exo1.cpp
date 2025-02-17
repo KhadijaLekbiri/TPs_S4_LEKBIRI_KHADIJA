5ced1da1f94fcbe857b16bf631e8a4fde6636e76
KhadijaLekbiri <lekbirikhadija3@gmail.com>
2025-02-17 02:12:40 +0100

    Delete TP1 directory

--- TP1/exo1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> // Includes the standard input/output library
-
-int main() {
-    // display statements
-    std::cout << "Hello, World! \n";
-    std::cout << "Bienvenue en C++!";
-    return 0; 
-}
